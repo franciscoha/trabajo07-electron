2878ce75f593d69ca387230a6ac52653635d4059
franciscoha <54969936+franciscoha@users.noreply.github.com>
2020-04-19 16:06:00 -0500

    first push

---
+++ src/index.js
@@ -0,0 +1,109 @@
+const { app, BrowserWindow, Menu, ipcMain } = require('electron')
+const url = require('url');
+const path = require('path');
+/*
+if(process.env.NODE_ENV!='production'){
+  require('electron-reload')(__dirname,{
+    electron: path.join(__dirname,'..node_modules','bin','electron')
+  })
+}*/
+
+
+let mainWindow;
+let newProductWindow;
+
+app.on('ready',()=>{
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+        nodeIntegration: true
+    }
+  })
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname,'./views/index.html'),
+    protocol:'file',
+    slashes:true
+  }))
+  const mainMenu = Menu.buildFromTemplate(templateMenu);
+  Menu.setApplicationMenu(mainMenu);
+  mainWindow.on('close',()=>{
+    app.quit();
+  })
+});
+
+ipcMain.on('product:new', (e,newProduct)=>{
+  mainWindow.webContents.send('product:new',newProduct);
+  newProductWindow.close();
+});
+
+const templateMenu = [
+  {
+    label:'Archivo',
+    submenu:[
+      {
+        label:'Nuevo producto',
+        accelerator:'Ctrl+N',
+        click(){
+          createNewProductWindow();
+        }
+      },
+      {
+        label:'Eliminar todos los productos',
+        click(){
+          mainWindow.webContents.send('products:removeAll');
+        }
+      },
+      {
+        label:'Salir',
+        accelerator: process.platform == 'darwin'?'command+Q':'Ctrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+if (process.platform==='darwin'){
+  templateMenu.unshift({
+    label:app.getName()
+  })
+}
+if(process.env.NODE_ENV!=='production'){
+  templateMenu.push({
+    label:'Herramientas de desarollo',
+    submenu:[
+      {
+        label:'mostrar/ocultar herramientas de desarollo',
+        accelerator:'Ctrl+D',
+        click(item,focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        label:'refresca ventana',
+        role:'reload'
+      }
+    ]
+  })
+}
+function createNewProductWindow(){
+  newProductWindow = new BrowserWindow({
+    width: 400,
+    height: 330,
+    title:'Agregar un nuevo producto',
+    webPreferences: {
+        nodeIntegration: true
+    }
+
+  });
+  
+  newProductWindow.setMenu(null)
+
+  newProductWindow.loadURL(url.format({
+    pathname: path.join(__dirname,'./views/nuevoProducto.html'),
+    protocol:'file',
+    slashes:true
+  }));
+  newProductWindow.on('close', ()=>{
+    newProductWindow = null;
+  })
+}
